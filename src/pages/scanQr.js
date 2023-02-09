84e32f4b698f7512ad3b680ac15d91086f06af27
BenedettaBottari <benedetta.bottari@studio.unibo.it>
2023-02-09 23:44:28 +0100

    add toast d'errore nelo scanner del reperto

--- src/pages/scanQr.js
+++ src/pages/scanQr.js
@@ -2,9 +2,13 @@
 import React, { useState } from 'react'
 import { QrReader } from 'react-qr-reader';
 import styles from '@/Home.module.css'
+import { Toast } from 'react-bootstrap';
+import {IoClose} from "react-icons/io5";
 
 export default function scanQR() { 
 
+  id=0
+  
   const [data, setData] = useState('No result');
 
   return (
@@ -15,12 +19,23 @@ export default function scanQR() {
         }}
         onResult={(result) => {
           if (!!result) {
-            setData(result?.text);
+            console.log('Read');
+            if(result?.text==id){
+              console.log('Enter')
+            }
           }
         }}
         className={styles.camera}
         style={{ width: '100%', height: '100%' }}
       />
+
+    <Toast>
+      <Toast.Header>
+        <IoClose className='redT'/>
+        <strong className="me-auto redT">Ritenta</strong>
+      </Toast.Header>
+      <Toast.Body>Il reperto trovato non è il tuo tesoro...</Toast.Body>
+    </Toast>
     </main>
   );
 };
