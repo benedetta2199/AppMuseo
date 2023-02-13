a60d137ca0800e495af72eb571f314b3da1e6bc9
BenedettaBottari <benedetta.bottari@studio.unibo.it>
2023-02-13 21:24:12 +0100

    fix error commit

--- src/pages/reperto.js
+++ src/pages/reperto.js
@@ -8,25 +8,56 @@ import db from '@database'
 
 export default function Reperto() {
   
-  
+  const r = useRouter();
+  //const {id, idRep, idUserRoute, index, lenght, dRep} = r.query;
+  const {id, idRep, idUserRoute, index, lenght} = r.query;
   
   const [text, setText] = useState('');
   const [color, setColore] = useState('');
   const [data, setData] = useState('');
   const [desc, setDesc] = useState('');
 
-   /*const incrementPoint = 10;
-const r = useRouter();
-  const {id, idRep, idUserRoute, index, lenght, dRep} = r.query;
- try {
+  const incrementPoint = 10;
+
+  try {
     const temp = JSON.parse(dRep);
     setText(temp.nome);
     setColore(temp.colore);
     setData(temp.anno);
     setDesc(temp.descrizione);
   } catch(e) {
-  }*/
+  }
   
+  /**AGGIRNA PUNTEGGIO E LA LISTA DEI REPERTI CON QUELLO APPENA SCOPERTO*/
+  const updateUser = async () => {
+    const refUser = doc(db, "user", id);
+    await updateDoc(refUser, {punteggio: increment(incrementPoint), reperti: arrayUnion(idRep)});
+  }
+  /**AGGIORNA IL PUNTEGGIO E L'INDICE RELATIVO AL PROSSIMO REPERTO DA VISUALIZZARE*/
+  const updatePercorsoFatto = async () => {
+    const refRoute = doc(db, "percorsoFatto", idUserRoute);
+    await updateDoc(refRoute, {punteggio: increment(incrementPoint),ultimoReperto: increment(1)});
+  }
+
+  /**CONTROLLA SE IL REPERTO è L'ULTIMO,
+   - IN CASO AFFERMATIVO MANDA ALL'ULTIMA PAGINA
+   - IN CASO NEGATIVO INDIRIZZA ALLA PAGINA DELL'INDIZIO SUCCESSIVO*/
+  const nextRep = async () => {
+    const i = index+1;
+    if(i==lenght){
+      const refRoute = doc(db, "percorsoFatto", idUserRoute);
+      await updateDoc(refRoute, {terminato: true, data: serverTimestamp()});
+      r.push({ pathname: './indizio', query: {id: id, idUserRoute: idUserRoute, index: i, lenght: lenght, idRep: next}});
+    }else{
+      r.push({ pathname: './indizio', query: {id: id, idUserRoute: idUserRoute, index: i, lenght: lenght, idRep: next}});
+    }
+  }
+
+  useEffect(()=>{
+    updateUser();
+    updatePercorsoFatto();
+
+  },[]);
 
   return (
     <main className={styles.main}>
@@ -39,7 +70,7 @@ const r = useRouter();
       </div>
 
       <button className={`${color} btn text-light t-abo mb-2`}>  Accumula più punti  </button>
-      <button className="btn text-light gray t-abo" >  Scopri il prossimo indizio </button>
+      <button className="btn text-light gray t-abo" onClick={()=>nextRep()}>  Scopri il prossimo indizio </button>
     </main>
   )
 }
