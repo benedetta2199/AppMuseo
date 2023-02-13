b05614f9a070168e546a7283562732c210e23df5
BenedettaBottari <benedetta.bottari@studio.unibo.it>
2023-02-13 22:32:34 +0100

    add page congratulation alla fine della caccia al tesoro

---
+++ src/pages/congratulation.js
@@ -0,0 +1,41 @@
+import { doc, getDoc } from 'firebase/firestore';
+import Confetti from 'react-confetti';
+import { useWindowSize } from '@react-hook/window-size';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+import db from '@database'
+import styles from '@/Home.module.css'
+
+export default function Home() {
+  const r = useRouter();
+  const {id} = r.query;
+
+  const[user,setUser]=useState('')  
+  const { width=400, height=800 } = useWindowSize();
+    
+
+  const loadUser = async () => {
+    const docRef = doc(db, "user", id);
+    const docSnap = await getDoc(docRef);
+    setUser(JSON.stringify(docSnap.data()));
+  }
+
+
+  useEffect(()=>{
+    loadUser();
+  },[]);
+
+  return (
+    <main className={`${styles.main} lock-height d-flex`}>
+      <Confetti width={Math.max(width,400)} height={Math.max(height,800)}/>
+      <div className='align-self-center'>
+        <h1>Complimenti!</h1>
+        <p className='mx-3'>Sei un vero esploratore<br/> Hai trovato il tesoro più prezioso: la conoscenza</p>
+        <button className='btn mx-auto gray whiteT t-abo' onClick={()=>r.push({ pathname: './indizio', query: {id: id, data: user}})}>
+            Torna alla home
+          </button>  
+      </div>
+    </main>
+  )
+}
